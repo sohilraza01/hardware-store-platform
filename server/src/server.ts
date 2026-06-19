44f6c468c2ab97ac4d6d3d16d48850d06cde8526
sohilraza01 <rsohil204@gmail.com>
2026-06-19 11:00:16 +0530

    Database-Updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,7 +37,7 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/upload', uploadRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on ${PORT}`);
 });
