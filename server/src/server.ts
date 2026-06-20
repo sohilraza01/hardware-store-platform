f37c7b88871c861993157783c90ee8efb60dccee
sohilraza01 <rsohil204@gmail.com>
2026-06-20 11:30:23 +0530

    fix:cors update

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,11 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://ahmadi-hardware.vercel.app',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://ahmadi-hardware.vercel.app',
+  ],
   credentials: true,
 }));
 app.use(express.json());
